c9f913730bf063234ca479c942a376706815099a
Nama Kamu <emailkamu@gmail.com>
2025-07-12 14:10:55 +0700

    Add login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'auth\AuthController::login');
+$routes->post('/login/submit', 'auth\AuthController::loginSubmit');
+$routes->get('/logout', 'auth\AuthController::logout');
--- app/Controllers/auth/AuthController.php
+++ app/Controllers/auth/AuthController.php
@@ -1,14 +1,46 @@
 <?php
 
-namespace App\Controllers;
+namespace App\Controllers\auth;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\UserModel;
 
 class AuthController extends BaseController
 {
-    public function index()
+    public function login()
     {
-        //
+        return view('auth/Login');
+    }
+
+    public function loginSubmit()
+    {
+        $session = session();
+        $model = new UserModel();
+
+        $email = $this->request->getPost('email');
+        $password = $this->request->getPost('password');
+
+        $user = $model->where('email', $email)->first();
+
+        if ($user) {
+            if (password_verify($password, $user['password'])) {
+                $session->set([
+                    'user_id' => $user['id'],
+                    'user_email' => $user['email'],
+                    'isLoggedIn' => true
+                ]);
+                return redirect()->to('/dashboard');
+            } else {
+                return redirect()->back()->with('error', 'Password salah.');
+            }
+        } else {
+            return redirect()->back()->with('error', 'Email tidak ditemukan.');
+        }
+    }
+
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('/login');
     }
 }
